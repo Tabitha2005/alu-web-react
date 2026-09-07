eae53f32533e28e77a60408a4ab6e60410dc5658
Tabitha2005 <Tabitha2005@users.noreply.github.com>
2026-09-07 17:14:22 +0100

    Fix task_3 logo image not rendering (double-asset and ES module URL bugs)

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,53 +1,58 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+	const path = require('path');
+	const HtmlWebpackPlugin = require('html-webpack-plugin');
+	const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-	mode: 'development',
-	entry: {
-		header: './modules/header/header.js',
-		body: './modules/body/body.js',
-		footer: './modules/footer/footer.js',
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'public')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
+	module.exports = {
+		mode: 'development',
+		entry: {
+			header: './modules/header/header.js',
+			body: './modules/body/body.js',
+			footer: './modules/footer/footer.js',
+		},
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'public')
+		},
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				},
+							{
 				test: /\.(png|jpe?g|gif)$/i,
+				type: 'javascript/auto',
 				use: [
-					'file-loader',
+					{
+						loader: 'file-loader',
+						options: {
+							esModule: false
+						},
+					},
 					{
 						loader: 'image-webpack-loader',
 						options: {
-							bypassOnDebug: true,
-							disable: true
+							disable: false
 						},
 					},
 				],
 			},
-		]
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: path.join(__dirname, './public'),
-		compress: true,
-		port: 8564
-	},
-	plugins: [
-		new CleanWebpackPlugin(), // clean the public folder before building
-		new HtmlWebpackPlugin() // Generates default index.html
-	],
-	optimization: {
-		splitChunks: {
-			chunks: 'all' // split all chunks
+			]
+		},
+		devtool: 'inline-source-map',
+		devServer: {
+			static: path.join(__dirname, './public'),
+			compress: true,
+			port: 8564
+		},
+		plugins: [
+			new CleanWebpackPlugin(), // clean the public folder before building
+			new HtmlWebpackPlugin() // Generates default index.html
+		],
+		optimization: {
+			splitChunks: {
+				chunks: 'all' // split all chunks
+			}
 		}
-	}
-	
-};
+
+	};
